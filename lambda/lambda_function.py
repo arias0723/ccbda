5468edd9b88522b84e27d1cc09b1a583087cb6ad
Jose Arias <jose.ricardo.arias@estudiantat.upc.edu>
2026-04-29 15:45:56 +0200

    initial lambda

---
+++ lambda/lambda_function.py
@@ -0,0 +1,41 @@
+import json
+import urllib3
+
+BOT_TOKEN = ""
+
+def sendReply(chat_id, message):
+    reply = {
+        "chat_id": chat_id,
+        "text": message
+    }
+
+    http = urllib3.PoolManager()
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    encoded_data = json.dumps(reply).encode('utf-8')
+    http.request('POST', url, body=encoded_data, headers={'Content-Type': 'application/json'})
+
+    print(f"*** Reply : {encoded_data}")
+
+
+def lambda_handler(event, context):
+    body = json.loads(event['body'])
+
+    print("*** Received event")
+
+    chat_id = body['message']['chat']['id']
+    user_name = body['message']['from']['username']
+    message_text = body['message']['text'] if 'text' in body['message'] else '<not available>'
+
+    print(f"*** chat id: {chat_id}")
+    print(f"*** user name: {user_name}")
+    print(f"*** message text: {message_text}")
+    print(json.dumps(body))
+
+    reply_message = f"Reply to: {user_name}. Message: {message_text}"
+
+    sendReply(chat_id, reply_message)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Message processed successfully')
+    }
